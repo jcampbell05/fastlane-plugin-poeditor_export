8f34bd416cfef50d79c00132cfd8e45d479f62de
James Campbell <james@supmenow.com>
2016-07-11 14:13:23 +0100

    Adds missing file.

---
+++ lib/fastlane/plugin/poeditor_export/helper/poeditor_export_helper.rb
@@ -0,0 +1,24 @@
+module Fastlane
+  module Helper
+    class PoeditorExportHelper
+      API_URL = 'https://poeditor.com/api/'
+      EXPORT_ACTION = 'export'
+
+      def self.export_for_lanaguage(params)
+        uri = URI(API_URL)
+
+        export_params = {
+          api_token: params[:api_token],
+          action: EXPORT_ACTION,
+          id: params[:project_id],
+          type: params[:export_format],
+          language: params[:language]
+        }
+
+        res = Net::HTTP.post_form(uri, export_params)
+        json = JSON.parse(res.body)
+        URI(json["item"])
+      end
+    end
+  end
+end
